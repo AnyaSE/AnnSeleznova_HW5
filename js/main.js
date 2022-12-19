d4d33b6c965288bd352247ac4b40612a03c13b20
AnyaSE <annseleznova@gmail.com>
2022-12-19 11:44:08 +0800

    wrote up js code for tasks 5 to 10

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const studentsArray = ['Olga', 'Paul', 'Michael', 'John', 'Bob', 'Rob', 'Kevin', 'Vicky', 'David', 'Karina'];
+
+studentsArray.push('Vova', 'Alfred');
+
+studentsArray.unshift('Prannay','Vijay');
+
+studentsArray.pop ();
+
+console.log(studentsArray);
+
+students.shift();
+
+console.log(studentsArray);
+
+console.log(studentsArray.includes('Vova'));
+
