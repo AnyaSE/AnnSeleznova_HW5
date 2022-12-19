a26f3a082f08635d727d2973f63a4f0603716ca3
AnyaSE <annseleznova@gmail.com>
2022-12-19 11:45:01 +0800

    completed js code for tasks 5 to 10

--- js/main.js
+++ js/main.js
@@ -8,9 +8,30 @@ studentsArray.pop ();
 
 console.log(studentsArray);
 
-students.shift();
+studentsArray.shift();
 
 console.log(studentsArray);
 
 console.log(studentsArray.includes('Vova'));
+
+const studentsArrayNew = studentsArray.slice(4,8);
+
+console.log(studentsArrayNew);
+
+const studentsArrayUnique = ['Carry', 'Viktoria', 'Cleyton', 'Bridget', 'Connie'];
+
+const studentsArrayAll = studentsArrayNew.concat(studentsArrayUnique); 
+
+console.log(studentsArrayAll);
+
+studentsArrayAll.splice(3,1);
+
+console.log(studentsArrayAll);
+
+studentsArrayAll.splice(5,1,'Ivan');
+
+console.log(studentsArrayAll);
+
+console.log(studentsArrayAll.join(' - '));
+
 
